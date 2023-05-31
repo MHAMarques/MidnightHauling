a40478e13e9c1d86b6d692cc10756b18100fc703
Marcelo Henrique Aguiar Marques <96503481+MHAMarques@users.noreply.github.com>
2023-05-31 13:38:35 -0300

    Initial Laravel Structure
    First steps towards a Laravel App project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @php
+    $avatar = $company['avatar'];
+    $newAvatar = str_replace("public/", "storage/", $avatar);
+@endphp
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{$company['name']}}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="icon" href="https://e.truckyapp.com/{{$newAvatar}}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<body class="antialiased">
+    <img width="40" height="40" src="https://e.truckyapp.com/{{$newAvatar}}" />
+   
+    <h1 class="text-xl font-bold">{{$company['name']}}</h1>
+    <h2>{{$company['company_type']}}
+
+    {{-- @foreach ($cards as $card)
+        <h2>{{$card['id']}}. {{$card['title']}}</h2>
+        {{$card['origem']}} ==>> {{$card['destino']}}
+        <br /><br />
+    @endforeach --}}
+</body>
+</html>
